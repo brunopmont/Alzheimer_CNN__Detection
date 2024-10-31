aeee113322acc4ec93b0dd7ee5d548eceaa31405
brunopmont <bpmonteiro@id.uff.br>
2024-10-31 11:24:47 -0300

    cnn_model update

--- src/cnn_network.py
+++ src/cnn_network.py
@@ -2,12 +2,13 @@ import numpy as np
 import nibabel as nib
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.preprocessing import LabelEncoder
-from tensorflow.keras.metrics import Precision, Recall
+from sklearn.utils import shuffle
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv3D, MaxPooling3D, Flatten, Dense, Dropout, BatchNormalization
+from tensorflow.keras.layers import Input, Conv3D, MaxPooling3D, Flatten, Dense, Dropout, BatchNormalization, Activation
 from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
 import matplotlib.pyplot as plt
 import os
+from tensorflow.keras.optimizers import Adam
 
 # Função para carregar imagens NIfTI, seus rótulos e cortar as imagens
 def load_nifti_images_with_labels(base_dir, slice_idx):
@@ -33,77 +34,63 @@ def load_nifti_images_with_labels(base_dir, slice_idx):
 # Função para criar o modelo
 def create_model(input_shape):
     model = Sequential([
-        Conv3D(32, (3, 3, 3), activation='relu', padding='same', input_shape=input_shape),
-        MaxPooling3D(pool_size=(2, 2, 2)),
+        Input(shape=input_shape),
+
+        # Camada 1
+        Conv3D(32, (3, 3, 3), padding='same'),
         BatchNormalization(),
-        
-        Conv3D(64, (3, 3, 3), activation='relu', padding='same'),
-        MaxPooling3D(pool_size=(2, 2, 2)),
+        Activation('relu'),
+        MaxPooling3D(pool_size=(2, 2, 2), padding='same'),
+
+        # Camada 2
+        Conv3D(64, (3, 3, 3), padding='same'),
         BatchNormalization(),
-        
-        Conv3D(128, (3, 3, 3), activation='relu', padding='same'),
-        MaxPooling3D(pool_size=(2, 2, 2)),
+        Activation('relu'),
+        MaxPooling3D(pool_size=(2, 2, 2), padding='same'),
+
+        # Camada 3
+        Conv3D(128, (3, 3, 3), padding='same'),
         BatchNormalization(),
-        
-        Conv3D(256, (3, 3, 3), activation='relu', padding='same'),
-        MaxPooling3D(pool_size=(2, 2, 2)),
+        Activation('relu'),
+        Conv3D(128, (3, 3, 3), padding='same'),
         BatchNormalization(),
-        
+        Activation('relu'),
+        MaxPooling3D(pool_size=(2, 2, 2), padding='same'),
+
+        # Camada 4
+        Conv3D(256, (3, 3, 3), padding='same'),
+        BatchNormalization(),
+        Activation('relu'),
+        Conv3D(256, (3, 3, 3), padding='same'),
+        BatchNormalization(),
+        Activation('relu'),
+        MaxPooling3D(pool_size=(2, 2, 2), padding='same'),
+
+        # Camada 5
+        Conv3D(256, (3, 3, 3), padding='same'),
+        BatchNormalization(),
+        Activation('relu'),
+        Conv3D(256, (3, 3, 3), padding='same'),
+        BatchNormalization(),
+        Activation('relu'),
+        MaxPooling3D(pool_size=(2, 2, 2), padding='same'),
+
+        # Flatten e camadas densas
         Flatten(),
         Dense(512, activation='relu'),
-        Dropout(0.5),
+        BatchNormalization(),
+        Dropout(0.2),
+
+        Dense(512, activation='relu'),
+        BatchNormalization(),
+        Dropout(0.2),
+
+        # Camada de saída
         Dense(3, activation='softmax')  # 3 classes: CN, MCI, AD
     ])
     return model
 
-# Definindo caminhos
-base_dir = os.getenv('BASE_DIR', '/default/base/path')
-train_dir = os.path.join(base_dir, 'ADNI1_ADNI2', 'train')
-val_dir = os.path.join(base_dir, 'ADNI1_ADNI2', 'validation')
-results_dir = os.path.join(base_dir, 'ADNI1_ADNI2', 'results')
-
-# Índices de corte para o corte NIfTI
-SLICE_NII_IDX0 = slice(24, 169)
-SLICE_NII_IDX1 = slice(24, 206)
-SLICE_NII_IDX2 = slice(6, 161)
-
-# Criar o diretório de resultados se ele não existir
-os.makedirs(results_dir, exist_ok=True)
-
-# Carregar os dados com cortes
-train_images, train_labels, class_labels = load_nifti_images_with_labels(train_dir, 
-                                                                        (SLICE_NII_IDX0, SLICE_NII_IDX1, SLICE_NII_IDX2))
-val_images, val_labels, _ = load_nifti_images_with_labels(val_dir, 
-                                                           (SLICE_NII_IDX0, SLICE_NII_IDX1, SLICE_NII_IDX2))
-
-# Adicionar a dimensão do canal
-train_images = train_images.reshape((-1, *train_images.shape[1:], 1))
-val_images = val_images.reshape((-1, *val_images.shape[1:], 1))
-
-# Criar e compilar o modelo
-input_shape = (145, 182, 155, 1)  # Formato para dados em escala de cinza
-model = create_model(input_shape)
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy', Precision(), Recall()])
-
-# Callbacks de treinamento
-reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5, min_lr=1e-6)
-early_stopping = EarlyStopping(monitor='val_loss', patience=10, verbose=1, restore_best_weights=True)
-
-# Treinamento do modelo
-history = model.fit(
-    train_images, 
-    train_labels,
-    epochs=100,
-    verbose=1,
-    validation_data=(val_images, val_labels),
-    callbacks=[early_stopping, reduce_lr],
-    batch_size=32
-)
-
-# Salva o modelo
-model.save(os.path.join(results_dir, "trained_model.h5"))
-
-# Função para avaliação do modelo
+# Função para avaliação
 def evaluate_model(val_images, val_labels, model, results_dir):
     predictions = model.predict(val_images)
     predicted_classes = np.argmax(predictions, axis=1)
@@ -115,15 +102,27 @@ def evaluate_model(val_images, val_labels, model, results_dir):
     print("Classification Report:\n", classification_report(val_labels, predicted_classes, target_names=class_labels))
     
     conf_matrix = confusion_matrix(val_labels, predicted_classes)
+    
+    # Normalizar a matriz de confusão para porcentagens
+    conf_matrix_normalized = conf_matrix.astype('float') / conf_matrix.sum(axis=1)[:, np.newaxis]
     np.savetxt(os.path.join(results_dir, "confusion_matrix.txt"), conf_matrix, fmt='%d')
     
     plt.figure(figsize=(8, 6))
-    plt.imshow(conf_matrix, interpolation='nearest', cmap=plt.cm.Blues)
+    plt.imshow(conf_matrix_normalized, interpolation='nearest', cmap=plt.cm.Blues)
     plt.title('Confusion Matrix')
     plt.colorbar()
+    
     tick_marks = np.arange(len(class_labels))
     plt.xticks(tick_marks, class_labels, rotation=45)
     plt.yticks(tick_marks, class_labels)
+    
+    # Adicionando os rótulos com porcentagens
+    thresh = conf_matrix_normalized.max() / 2.
+    for i, j in np.ndindex(conf_matrix_normalized.shape):
+        plt.text(j, i, f'{conf_matrix_normalized[i, j]:.2%}', 
+                 horizontalalignment="center",
+                 color="white" if conf_matrix_normalized[i, j] > thresh else "black")
+    
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
     plt.tight_layout()
@@ -138,8 +137,6 @@ def evaluate_model(val_images, val_labels, model, results_dir):
     print(f"Acurácia: {accuracy:.4f}")
     print(f"Recall: {recall:.4f}")
     print(f"F1 Score: {f1_score:.4f}")
-
-evaluate_model(val_images, val_labels, model, results_dir)
 
 # Função para plotar e salvar o histórico de treinamento
 def plot_history(history, results_dir):
@@ -164,5 +161,74 @@ def plot_history(history, results_dir):
     plt.tight_layout()
     plt.savefig(os.path.join(results_dir, "training_history.png"))
     plt.close()
+
+# Salvar hiperparâmetros
+def save_hyperparameters(model):
+    results_dir = 'results'
+    n = len(os.listdir(results_dir))
+    folder_name = "test_" + str(n+1)
+    results_dir = os.path.join(results_dir, folder_name)
+
+    with open(os.path.join(results_dir, "model_hyperparameters.txt"), "w") as f:
+            f.write(model.get_config())
+
+
+# Definindo caminhos
+train_dir = 'train'
+val_dir = 'validation'
+results_dir = 'results'
+
+# Índices de corte para o corte NIfTI
+SLICE_NII_IDX0 = slice(24, 169)
+SLICE_NII_IDX1 = slice(24, 206)
+SLICE_NII_IDX2 = slice(6, 161)
+
+# Criar o diretório de resultados se ele não existir
+os.makedirs(results_dir, exist_ok=True)
+
+# Carregar os dados com cortes
+train_images, train_labels, class_labels = load_nifti_images_with_labels(train_dir, 
+                                                                        (SLICE_NII_IDX0, SLICE_NII_IDX1, SLICE_NII_IDX2))
+val_images, val_labels, _ = load_nifti_images_with_labels(val_dir, 
+                                                           (SLICE_NII_IDX0, SLICE_NII_IDX1, SLICE_NII_IDX2))
+
+# Adicionar a dimensão do canal
+train_images = train_images.reshape((-1, *train_images.shape[1:], 1))
+val_images = val_images.reshape((-1, *val_images.shape[1:], 1))
+
+# Embaralhar os dados
+train_images, train_labels = shuffle(train_images, train_labels, random_state=42)
+val_images, val_labels = shuffle(val_images, val_labels, random_state=42)
+
+# Taxa de aprendizado
+learning_rate = 1e-5
+optimizer = Adam(learning_rate=learning_rate)
+
+# Compila model
+input_shape = (145, 182, 155, 1)
+model = create_model(input_shape)
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# Callbacks
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5, min_lr=1e-6)
+early_stopping = EarlyStopping(monitor='val_loss', patience=10, verbose=1, restore_best_weights=True)
+
+# Treinamento
+history = model.fit(
+    train_images, 
+    train_labels,
+    epochs=50,
+    verbose=1,
+    validation_data=(val_images, val_labels),
+    callbacks=[early_stopping, reduce_lr],
+    batch_size=16
+)
+
+# Salva o modelo
+model.save(os.path.join(results_dir, "trained_model.h5"))
+
+save_hyperparameters(model)
+
+evaluate_model(val_images, val_labels, model, results_dir)
 
 plot_history(history, results_dir)
