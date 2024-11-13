5558210aa57aa30f7f26c8300bef80dcfb945a19
brunopmont <bpmonteiro@id.uff.br>
2024-11-13 17:03:40 -0300

    binary classification loading data via chunks

--- src/dicom_to_nii.py
+++ src/dicom_to_nii.py
@@ -2,6 +2,7 @@ import os
 import SimpleITK as sitk
 from concurrent.futures import ProcessPoolExecutor
 import sys
+from datetime import datetime
 
 # FUNÇÕES
 def get_f_dir(directory):
@@ -25,12 +26,12 @@ def reorient_image(image):
 
 def convert_dicom_to_nifti(input_folder, output_folder):
     # Formar nome de saída pelo 'S_...'
-    output_name = os.path.basename(input_folder) + '.nii.gz'
-
-    input_folder = get_f_dir(get_f_dir(os.path.join(input_folder, 'MPRAGE')))
-
-    # Formar nome de saída peçp 'I...'
     #output_name = os.path.basename(input_folder) + '.nii.gz'
+
+    #input_folder = get_f_dir(os.path.join(input_folder, 'MPRAGE'))
+
+    # Formar nome de saída pelo 'I...'
+    output_name = os.path.basename(input_folder) + '.nii.gz'
 
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
@@ -53,28 +54,50 @@ def convert_dicom_to_nifti(input_folder, output_folder):
 # CONVERSÃO
 if __name__ == "__main__":
 
+    tot_images = 0
+
     DIR_BASE = ''
     DIR_RAW = ''
 
     # DIRETÓRIOS
-    if len(sys.argv < 1):
-        DIR_BASE = os.path.abspath('/mnt/e/ADNI_8k/1.7K - REPEAT')
-        DIR_RAW = os.path.join('/mnt/e/full_dataset', os.path.basename(DIR_BASE))
-    else:
+    if len(sys.argv) > 1:
         DIR_BASE = sys.argv[1] #Endereço do diretório com as subpastas que contem as pastas dos arquivos dicom
         DIR_RAW = os.path.join(sys.argv[2], os.path.basename(DIR_BASE)) #endereço do diretório de Saída
+    else:
+        DIR_BASE = os.path.abspath("C:/Users/Team Taiane/Desktop/ADNI/DICOM/MPRAGE_SENSE2")
+        DIR_RAW = os.path.join("C:/Users/Team Taiane/Desktop/ADNI/RAW", os.path.basename(DIR_BASE))
 
-    for names in DIR_BASE:
+    os.makedirs(DIR_RAW, exist_ok=True)
+
+    print(f"Convertendo imagens de:\n{DIR_BASE}\npara:\n{DIR_RAW}")
+
+    start_time = datetime.now()
+    print(f"Início do processamento em: {start_time}")
+
+    for names in os.listdir(DIR_BASE):
         print(f"CONVERSOES DA PASTA {names}")
 
-        input_folder = os.path.abspath(os.path.join(DIR_BASE, names, 'ADNI'))
+        input_folder = os.path.join(DIR_BASE, names, 'ADNI')
 
-        output_folder = os.path.abspath(os.path.join(DIR_RAW, names))
+        output_folder = os.path.join(DIR_RAW, names)
+
+        os.makedirs(output_folder, exist_ok=True)
 
         # Coletar todas as pastas DICOM
-        dicom_folders = [os.path.join(input_folder, folder) for folder in os.listdir(input_folder)]
+        already_converted = [file for file in os.listdir(output_folder)]
+        dicom_folders = []
 
-        with ProcessPoolExecutor(16) as executor:
+        for folder in os.listdir(input_folder):
+            subdir = os.path.join(input_folder, folder, os.path.basename(DIR_BASE))
+            for subsubdir in os.listdir(subdir):
+                subsubdir = os.path.join(subdir, subsubdir)
+                for item in os.listdir(subsubdir):
+                    file = os.path.join(subsubdir, item)
+                    if os.path.basename(file) + '.nii.gz' not in already_converted:
+                        dicom_folders.append(file)
+
+
+        with ProcessPoolExecutor(8) as executor:
             futures = {executor.submit(convert_dicom_to_nifti, folder, output_folder): folder for folder in dicom_folders}
             
             for future in futures:
@@ -83,4 +106,12 @@ if __name__ == "__main__":
                 except Exception as e:
                     print(f"Erro ao processar {futures[future]}: {e}")
 
-        print(f'Foram convertidas {len(dicom_folders)} imagens!')
+        tot_images += len(dicom_folders)
+
+        print(f'\nForam convertidas {len(dicom_folders)} imagens!')
+
+    # Fim do processamento
+    print(f'\nForam convertidas {len(dicom_folders)} imagens!')
+    end_time = datetime.now()
+    print(f"Término do processamento em: {end_time}")
+    print(f"Duração total: {end_time - start_time}")
--- src/pre_process_parallel.py
+++ src/pre_process_parallel.py
@@ -16,75 +16,93 @@ logger = logging.getLogger()
 
 # FUNÇÕES
 # Winsorize -> reduz outliers, limitando os percentis inf e sup
-def winsorize_image(image_data, lower_percentile=1, upper_percentile=99):
+def winsorize_image(image_data, lower_percentile=1, upper_percentile=99): #reduz valores extremos
     lower_bound = np.percentile(image_data, lower_percentile)
     upper_bound = np.percentile(image_data, upper_percentile)
     winsorized_data = np.clip(image_data, lower_bound, upper_bound)
     return winsorized_data
 
 # Normalization -> valores de voxels entre 0 e 1
-def normalize_image(image_data):
+def normalize_image_min(image_data): 
     min_val = np.min(image_data)
     max_val = np.max(image_data)
     normalized_data = (image_data - min_val) / (max_val - min_val)
     return normalized_data
 
+def normalize_image_mean_std(image_data): #normaliza pela média e desvio padrão
+    mean_val = np.mean(image_data)
+    std_val = np.std(image_data)
+    normalized_data = (image_data - mean_val) / std_val  # Normaliza para média 0 e desvio padrão 1
+    return normalized_data
+
+def clip_image(image_data, lower_percentile=2, upper_percentile=98):
+    lower = np.percentile(image_data, lower_percentile)
+    upper = np.percentile(image_data, upper_percentile)
+    clipped_data = np.clip(image_data, lower, upper)
+    return clipped_data
+
 # Função para processar uma única imagem
 def process_image(img_path, template, mask):
     try:
-        logger.info(f"Inicio processamento: {img_path}")
+        logger.info(f"Inicio processamento: {os.path.basename(img_path)}")
         # Carrega a imagem
         image = ants.image_read(img_path)
-        data = image.numpy()
-        
-        # Winsorizing
-        data = winsorize_image(data)
-
-        # Bias Field Correction
-        image = ants.from_numpy(data, origin=image.origin, spacing=image.spacing, direction=image.direction)
-        image = ants.n4_bias_field_correction(image, shrink_factor=2)
-
-        # Reaplica winsorizing
-        data = image.numpy()
-        data = winsorize_image(data)
-        image = ants.from_numpy(data, origin=image.origin, spacing=image.spacing, direction=image.direction)
 
         # Registro (Registration) com as transformações para a imagem e máscara, com intuito de melhorar o corte
         registration = ants.registration(fixed=template, moving=image, type_of_transform='Translation')
         warped_image = registration['warpedmovout']
+        #logger.info(f"TRANSLATION")
 
         registration = ants.registration(fixed=template, moving=warped_image, type_of_transform='Rigid')
         warped_image = registration['warpedmovout']
+        #logger.info(f"RIGID")
 
         registration = ants.registration(fixed=template, moving=warped_image, type_of_transform='Affine')
         warped_image = registration['warpedmovout']
+        #logger.info(f"AFFINE")
 
-        registration = ants.registration(fixed=template, moving=warped_image, type_of_transform='SyN')
+        registration = ants.registration(fixed=template, moving=image, type_of_transform='SyN')
         warped_image = registration['warpedmovout']
+        #logger.info(f"SYN")
 
         # Máscara do cérebro e extração
         brain_masked = ants.mask_image(warped_image, mask)
+        data = brain_masked.numpy()
+
+        # Winsorizing
+        data = winsorize_image(data, lower_percentile=2, upper_percentile=99)
+        #logger.info(f"WINSORIZE")
+
+        # Bias Field Correction
+        image = ants.from_numpy(data, origin=image.origin, spacing=image.spacing, direction=image.direction)
+        image = ants.n4_bias_field_correction(image, shrink_factor=2)
+        #logger.info(f"BIAS")
+
+        # Reaplica winsorizing
+        data = image.numpy()
+        data = winsorize_image(data, lower_percentile=2, upper_percentile=99)
+        image = ants.from_numpy(data, origin=image.origin, spacing=image.spacing, direction=image.direction)
+        #logger.info(f"WINSORIZE AGAIN")
 
         # Normalização
-        normalized_data = normalize_image(brain_masked.numpy())
+        normalized_data = normalize_image_min(brain_masked.numpy())
         normalized_image = ants.from_numpy(normalized_data, origin=brain_masked.origin, spacing=brain_masked.spacing, direction=brain_masked.direction)
 
-        logger.info(f"Imagem {img_path} processada.")
+        #logger.info(f"Imagem {os.path.basename(img_path)} processada.")
         return normalized_image
         
     except Exception as e:
-        logger.error(f"Erro ao processar a imagem {img_path}: {e}")
+        logger.error(f"Erro ao processar a imagem {os.path.basename(img_path)}: {e}")
         return None
 
 # Função que processa e salva uma imagem
-def process_and_save_image(img_path, template, mask, output_dir, subfolder):
-    normalized_image = process_image(img_path, template, mask)
+def process_and_save_image(img_path, template, mask, output_dir):
+    normalized_image = process_image(img_path, template, mask) #processa imagem
     if normalized_image is not None:
-        output_path = os.path.join(output_dir, subfolder)
-        os.makedirs(output_dir, exist_ok=True)
-        output_path = os.path.join(output_path, os.path.basename(img_path))
+        os.makedirs(output_dir, exist_ok=True) #cria o diretório onde será salvo caso não exista
+        output_path = os.path.join(output_dir, os.path.basename(img_path)) 
         ants.image_write(normalized_image, output_path)
-        logger.info(f"Imagem salva: {output_path}")
+        logger.info(f"Imagem salva: {os.path.basename(output_path)}")
         # Liberar a memória manualmente
         normalized_image = None
         gc.collect()
@@ -92,47 +110,58 @@ def process_and_save_image(img_path, template, mask, output_dir, subfolder):
 # Início do processamento
 if __name__ == "__main__":
     # DIRETÓRIOS
-    DIR_RAW = ''
-    DIR_OUTPUT = ''
-    DIR_MASK = ''
+    DIR_RAW = '' #diretório raiz de onde as imagens estão não processadas
+    DIR_OUTPUT = '' #diretório onde as imagens serão processadas
+    DIR_MASK = '' #diretório com as máscaras
 
-    if len(sys.argv < 1):
-        DIR_RAW = os.path.abspath('/mnt/e/ADNI_8k/1.7K - REPEAT')
-        DIR_OUTPUT = os.path.abspath('/mnt/e/full_nii_adni')
-        DIR_MASK = os.path.abspath('/mnt/e/ADNI/ADNI2/mni_icbm152_nlin_asym_09c')
-    else:
+    if len(sys.argv) > 2: #passar endereços pela linha de comando
         DIR_RAW = sys.argv[1]
         DIR_OUTPUT = sys.argv[2]
         DIR_MASK = sys.argv[3]
-
+    else: #passar os endereços aqui mesmo
+        DIR_RAW = os.path.abspath("/mnt/c/Users/Team Taiane/Desktop/ADNI/ADNI_extended/RAW/MP-RAGE")
+        DIR_OUTPUT = os.path.abspath(os.path.join("/mnt/c/Users/Team Taiane/Desktop/ADNI/ADNI_extended/PROCESSED", os.path.basename(DIR_RAW)))
+        os.makedirs(DIR_OUTPUT, exist_ok=True)
+        DIR_MASK = os.path.abspath("/mnt/c/Users/Team Taiane/Desktop/ADNI/mni_icbm152_nlin_asym_09c_nifti/mni_icbm152_nlin_asym_09c")
+    
     template_path = os.path.join(DIR_MASK, 'mni_icbm152_t1_tal_nlin_asym_09c.nii')
     mask_path = os.path.join(DIR_MASK, 'mni_icbm152_t1_tal_nlin_asym_09c_mask.nii')
 
-    template = ants.image_read(template_path)
-    mask = ants.image_read(mask_path)
+    template = ants.image_read(template_path) 
+    mask = ants.image_read(mask_path) 
 
     start_time = datetime.now()
-    logger.info(f"Início do processamento em: {start_time}")
+    logger.info(f"INICIO DO PROCESSAMENTO")
 
-    for names in os.listdir(DIR_RAW):
+    for names in os.listdir(DIR_RAW): #itera as subpastas dentre o diretóiro original
+
+        '''
+        se for só um diretório com todas as imagens, 
+        pode tirar esse for e deixar input path e output path 
+        só com DIR RAW e DIR OUTPUT e tirar os.makedirs
+        '''
 
         input_path = os.path.join(DIR_RAW, names)
+        output_path = os.path.join(DIR_OUTPUT, names)
+        os.makedirs(output_path, exist_ok=True)
 
-        # Lista de caminhos para as imagens brutas
-        already_processed = [file for file in os.listdir(DIR_OUTPUT)]
-        image_paths = [os.path.join(input_path, file) for file in os.listdir(input_path) if file not in already_processed]
+        # Caminhos das imagens
+        already_processed = [file for file in os.listdir(output_path)] #checa o diretório de saída pra ver se alguma imagem já foi processada
+        image_paths = [os.path.join(input_path, file) for file in os.listdir(input_path) if file not in already_processed] #carrega o endereço das imagens não processadas
 
         # Função parcial para passar parâmetros fixos
-        process_func = partial(process_and_save_image, template=template, mask=mask, output_dir=DIR_OUTPUT, subfolder=names)
+        process_func = partial(process_and_save_image, template=template, mask=mask, output_dir=output_path)
 
         # Processamento e salvamento de cada imagem usando ProcessPoolExecutor
-        with ProcessPoolExecutor(max_workers=4) as executor:
+        with ProcessPoolExecutor(max_workers=4) as executor: #max_workers define o número máximo de processos paralelos
+            # dependendo do pc, é melhor fazer um por vez, pois paralelizar pode deixar cada processo mais demorado sem hardware que aguente
             futures = [executor.submit(process_func, img_path) for img_path in image_paths]
             
             for future in as_completed(futures):
                 future.result()  # Pega o resultado para garantir que exceções sejam lançadas
 
+
     # Fim do processamento
     end_time = datetime.now()
-    logger.info(f"Término do processamento em: {end_time}")
+    logger.info(f"FIM DO PROCESSAMENTO")
     logger.info(f"Duração total: {end_time - start_time}")
